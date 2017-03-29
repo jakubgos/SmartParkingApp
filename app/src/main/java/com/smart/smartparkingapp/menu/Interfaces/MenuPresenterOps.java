2437a4ac8dee92ecde28c066f645bdf48b25336b
Jakub Gos <jackobxy@gmail.com>
2017-03-29 18:23:59 +0200

    lettercase fix

---
+++ app/src/main/java/com/smart/smartparkingapp/menu/Interfaces/MenuPresenterOps.java
@@ -0,0 +1,8 @@
+package com.smart.smartparkingapp.menu.interfaces;
+
+/**
+ * Created by Bos on 2017-03-09.
+ */
+public interface MenuPresenterOps {
+    void onMapSelect();
+}
---
+++ app/src/main/java/com/smart/smartparkingapp/menu/Interfaces/MenuViewOps.java
@@ -0,0 +1,9 @@
+package com.smart.smartparkingapp.menu.interfaces;
+
+/**
+ * Created by Bos on 2017-03-09.
+ */
+
+public interface MenuViewOps {
+    void loadMap();
+}
